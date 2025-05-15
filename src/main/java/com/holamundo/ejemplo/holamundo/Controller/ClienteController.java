95da488a8f96e78e4e63cbf5f4495733ee7da6b4
Felipe Becerra <felipe.e.becerra@gmail.com>
2025-05-15 16:56:58 -0400

    Prueba modificación ClienteController pt.2

--- src/main/java/com/holamundo/ejemplo/holamundo/Controller/ClienteController.java
+++ src/main/java/com/holamundo/ejemplo/holamundo/Controller/ClienteController.java
@@ -6,4 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@RestController
+@RequestMapping("/api/v1/clientes")
+public class ClienteController {
 
+    @GetMapping
+    public String index(){
+        return "Bienvenidos el listado de clientes";
+    }
+
+}
